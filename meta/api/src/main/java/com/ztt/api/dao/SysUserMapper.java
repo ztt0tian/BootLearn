e16acc59e8e681343a74d3356672257529fc0ac3
ztt <whutzhaotian@gmail.com>
2021-11-14 13:52:55 +0800

    init

---
+++ meta/api/src/main/java/com/ztt/api/dao/SysUserMapper.java
@@ -0,0 +1,17 @@
+package com.ztt.api.dao;
+
+import com.ztt.api.po.SysUser;
+
+public interface SysUserMapper {
+    int deleteByPrimaryKey(Long userId);
+
+    int insert(SysUser record);
+
+    int insertSelective(SysUser record);
+
+    SysUser selectByPrimaryKey(Long userId);
+
+    int updateByPrimaryKeySelective(SysUser record);
+
+    int updateByPrimaryKey(SysUser record);
+}
---
+++ meta/api/src/main/java/com/ztt/api/po/SysUser.java
@@ -0,0 +1,75 @@
+package com.ztt.api.po;
+
+import java.util.Date;
+
+public class SysUser {
+    private Long userId;
+
+    private String userName;
+
+    private String userPsw;
+
+    private String phone;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    private Date lastLoginTime;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName == null ? null : userName.trim();
+    }
+
+    public String getUserPsw() {
+        return userPsw;
+    }
+
+    public void setUserPsw(String userPsw) {
+        this.userPsw = userPsw == null ? null : userPsw.trim();
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone == null ? null : phone.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public Date getLastLoginTime() {
+        return lastLoginTime;
+    }
+
+    public void setLastLoginTime(Date lastLoginTime) {
+        this.lastLoginTime = lastLoginTime;
+    }
+}
